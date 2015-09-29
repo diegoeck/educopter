223f4f8781fd28b188ba8c641a50ee26a3843c2f
Diego Eckhard <diego@eckhard.com.br>
2015-09-29 12:01:21 -0300

    coloca filtro complementar 3d devolta

--- codigo.c
+++ codigo.c
@@ -205,7 +205,6 @@ void angulos() {
     angle[0]=_atan2(accADC[0],t[0]);
     angle[1]=_atan2(accADC[1],t[1]);
 
-/*
     
     t[0]=(float)gyroADC[0]*cos(anglef[1]/572.9)/sqrt(  1-square( sin(anglef[0]/572.9)*sin(anglef[1]/572.9)  )   );
     t[1]=(float)gyroADC[2]*sin(anglef[1]/572.9)/sqrt(  1-square( sin(anglef[0]/572.9)*cos(anglef[1]/572.9)  )   );
@@ -223,12 +222,12 @@ void angulos() {
     // Matlab code
     // v1(i+1)=0.01*aa(i)+0.99*v1(i)+0.99*( VarName1(i)*cos(v2(i))/sqrt(1-(sin(v1(i))*sin(v2(i)))^2)+VarName3(i)*sin(v2(i))/sqrt(1-(sin(v1(i))*cos(v2(i)))^2) )/m*pi/2 ;
     // v2(i+1)=0.01*bb(i)+0.99*v2(i)+0.99*( VarName2(i)*cos(v1(i))/sqrt(1+(sin(v2(i))*sin(v1(i)))^2)-VarName3(i)*sin(v1(i))/sqrt(1-(sin(v2(i))*cos(v1(i)))^2) )/m*pi/2 ;
- */
+ 
 
-    
+/*
     anglef[0]=0.01*(float)angle[0]+0.02754*(float)gyroADC[0]+0.99*anglef[0];
     anglef[1]=0.01*(float)angle[1]+0.02754*(float)gyroADC[1]+0.99*anglef[1];
-
+*/
     // 2000/8192*0.01*10 = 0.024414 //Multiwii *0.99
     // 4/14.375*0.01*10  = 0.02781  //Datasheer *0.99
 }
@@ -242,6 +241,7 @@ void controle() {
     float e;
     
     pid[2]=(ref[3]-gyroADC[2]);
+
 
     pid[0]=((ref[0]-(int)anglef[0])*2-gyroADC[0])*0.3;
     pid[1]=((ref[1]-(int)anglef[1])*2-gyroADC[1])*0.3;
